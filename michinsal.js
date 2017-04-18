4caf676d56f964e951ec7ff38c68b6c1e06862d3
hyukkyun <skywings38@gmail.com>
2017-04-18 10:35:11 +0900

    ss
    s

--- michinsal.js
+++ michinsal.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var source = "https://michinsal.github.io/bgm.mp3"
 	var audio = document.createElement("audio");
 	audio.src = source;
-	audio.play();    
+	//audio.play();    
 	audio.volume = 0.2;
 
 	    
